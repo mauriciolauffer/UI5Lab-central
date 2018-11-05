40cfa1abafc3428f1237d1b3f99fac937d3043c6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-05 12:25:46 +0000

    Deploy UI5Lab/UI5Lab-central to github.com/UI5Lab/UI5Lab-central.git:gh-pages

---
+++ browser/resources/openui5/password/library-dbg.js
@@ -0,0 +1,32 @@
+sap.ui.define([
+  'jquery.sap.global',
+  'sap/ui/core/library'
+], function(jQuery) {
+  'use strict';
+
+  /**
+   * OpenUI5 library: openui5.password.
+   *
+   * @namespace
+   * @name openui5.password
+   * @author Mauricio Lauffer
+   * @version 0.1.3
+   * @public
+   */
+  // delegate further initialization of this library to the Core
+  const openui5 = {};
+  sap.ui.getCore().initLibrary({
+    name: 'openui5.password',
+    dependencies: [
+      'sap.ui.core',
+      'sap.m'
+    ],
+    interfaces: [],
+    controls: ['openui5.password.Password'],
+    elements: [],
+    noLibraryCSS: true,
+    version: '0.1.3'
+  });
+
+  return openui5.password;
+}, /* bExport= */ false);
